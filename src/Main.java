e56a45e37d66b25e3c4cf1f5b9a9efb86f6d18e5
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-13 11:50:11 +0500

    SekToMinитд

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Привет, напиши сколько прошло секунд с начала суток: ");
+            int s;
+            try {
+                s = scanner.nextInt();
+
+            int h = (s - s % 3600) / 3600;
+            int m = ((s - h * 3600) - (s - h * 3600) % 60) / 60;
+            System.out.println("Прошло: Часов: " + h);
+            System.out.println("        Минут: " + m);
+            System.out.println("        Секунд: " + (s - h * 3600 - m * 60));
+            } catch (Exception ex) {
+                System.out.println("Ты ввёл что то не так");
+            }
+        }
 }
